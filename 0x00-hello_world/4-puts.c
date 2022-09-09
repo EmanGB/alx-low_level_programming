4fdc2aa82d476901662d956a57ac75b7dea1ee9a
EmanGB <barakae986@gmail.com>
2022-09-09 08:44:49 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,9 @@
-#include<stdio.h>
+#include <studio.h>
 
-/**
- * main - Entry point
- * description: 'printing to the stdout using puts function '
- * Return: Always 0 (Success)
- */
-
-int main(void)
-
+in main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-
-   puts("\"Programming is like building a multilingual puzzle");
-
-
-
-   return (0);
+	return (0);
 
 }
-
-
