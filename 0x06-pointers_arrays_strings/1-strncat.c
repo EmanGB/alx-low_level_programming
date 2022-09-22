988a902d1128c0216ff606f8203987103c980ed7
EmanGB <barakae986@gmail.com>
2022-09-22 11:31:22 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,53 @@
+#include <string.h>
+
+#include "main.h"
+
+
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest:  output file
+ *
+ * @src: input file
+ *
+ * Return: pointer dest
+ *
+ * @n: number of the source indexs to be appended
+ */
+
+
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+		int l1, i;
+
+
+
+			l1 = strlen(dest);
+
+
+
+
+
+				i = 0;
+
+					while (i < n && src[i] != '\0')
+
+							{
+
+										dest[l1 + i] = src[i];
+
+												i++;
+
+													}
+
+						dest[l1 + i] = '\0';
+
+
+
+							return (dest);
+
+}
